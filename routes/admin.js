8867ebf6a93df51184c4878d4ebb75d5e968c208
Ammar Kalim <ammar.kalim@gmail.com>
2018-06-19 19:09:04 -0400

    Add bootstrap, and pass in variable to template

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('admin', {});
+    res.render('admin', {title: 'Admin'});
 });
 
 module.exports = router;
